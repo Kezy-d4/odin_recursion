8b7b13a92ab42f21fb2890761c5095d5f8b37feb
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-09 18:26:16 -0400

    rewrite: iterative and recursive fibonacci methods
    Rewrote these methods to be more descriptive, easier to understand, and
    to count from 1 instead of 0 (F(1) = 0). Calling fibs(0) now just
    returns an empty array. Also edited the top code comment accordingly.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,23 +1,28 @@
 # The following two methods return the specified sequence of fibonacci numbers
-# counting from zero, not one. For example, fibs(3) returns [0, 1, 1, 2],
-# not [0, 1, 1]. The first method is iterative and the second method is
-# recursive.
+# assuming F(1) = 0. For example, fibs(4) returns [0, 1, 1, 2]. The first method
+# is iterative and the second method is recursive.
 
 def fibs(num)
   sequence = []
   iterator = 0
-  until iterator > num
-    sequence << iterator if (0..1).cover?(iterator)
+  until iterator == num
+    sequence << 0 if iterator.zero?
+    sequence << 1 if iterator == 1
     sequence << (sequence[-2] + sequence[-1]) if iterator > 1
     iterator += 1
   end
   sequence
 end
 
-def fibs_rec(num, iterator = 0, sequence = [])
-  return sequence if iterator > num
+def fibs_rec(num, sequence = [])
+  return sequence if sequence.length == num
 
-  sequence << iterator if (0..1).cover?(iterator)
-  sequence << (sequence[-2] + sequence[-1]) if iterator > 1
-  fibs_rec(num, iterator + 1, sequence)
+  if sequence.empty?
+    sequence << 0
+  elsif sequence.length == 1
+    sequence << 1
+  elsif sequence.length > 1
+    sequence << (sequence[-2] + sequence[-1])
+  end
+  fibs_rec(num, sequence)
 end
