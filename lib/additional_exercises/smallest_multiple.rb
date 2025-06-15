a4243fc56911aa3816c36000fc6956339f7c0152
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-15 01:20:36 -0400

    wip: lcm

---
+++ lib/additional_exercises/smallest_multiple.rb
@@ -0,0 +1,8 @@
+# Write a recursive method that finds the smallest positive number that is
+# evenly divisible by each number from one through twenty.
+
+def smallest_multiple
+end
+
+# Driver code
+p smallest_multiple # => 232_792_560
